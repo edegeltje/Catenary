37152896fa80a72387df60830e86da44197aa1e8
Alvise Sembenico <alvise.sembenico@gmail.com>
2025-05-15 16:33:41 +0200

    feat: update .gitignore and enhance Codim.lean with additional imports and lemmas

--- Catenary/Topology/Codim.lean
+++ Catenary/Topology/Codim.lean
@@ -1,4 +1,5 @@
 import Catenary.RelSeriesHT.Defs
+import Catenary.RelSeriesHT.Codim
 import Mathlib.Topology.Sober
 import Catenary.Order.Defs
 
@@ -6,9 +7,11 @@ import Catenary.Order.Defs
 open TopologicalSpace Set.Notation RelSeriesHT
 variable {X : Type*} [TopologicalSpace X] {U : Set X}
 
-lemma closure_irred_of_irred {U : Set X} (U_nonempty : U.Nonempty) (T : Set U) (T_irred : IsIrreducible T) : IsIrreducible (closure (X := X) T) := sorry
+lemma closure_irred_of_irred {U : Set X} (U_nonempty : U.Nonempty) (T : Set U) (T_irred : IsIrreducible T) : IsIrreducible (closure (X := X) T) := by
+ sorry
 
-def closure_irred {U : Set X} (U_nonempty : U.Nonempty) (U_open : IsOpen U) : IrreducibleCloseds U → {s : IrreducibleCloseds X // ((s : Set X) ∩ U).Nonempty} := fun T ↦ ⟨⟨closure (X := X) T.carrier, closure_irred_of_irred U_nonempty T.carrier T.is_irreducible', isClosed_closure⟩, sorry⟩
+def closure_irred {U : Set X} (U_nonempty : U.Nonempty) (U_open : IsOpen U) : IrreducibleCloseds U → {s : IrreducibleCloseds X // ((s : Set X) ∩ U).Nonempty} :=
+  fun T ↦ ⟨⟨closure (X := X) T.carrier, closure_irred_of_irred U_nonempty T.carrier T.is_irreducible', isClosed_closure⟩, sorry⟩
 
 def inter_irred (hU : IsOpen U) : {s : (IrreducibleCloseds X) // (U ↓∩ s).Nonempty } → IrreducibleCloseds U :=
   fun x =>
@@ -20,12 +23,15 @@ def inter_irred (hU : IsOpen U) : {s : (IrreducibleCloseds X) // (U ↓∩ s).No
       rw [isPreirreducible_iff_isClosed_union_isClosed] at this ⊢
       intro z₁ z₂ hz₁ hz₂ hunion
       specialize this (closure z₁) (closure z₂) (isClosed_closure) isClosed_closure
-      rw [← closure_union, ← Set.image_union] at this
+      -- rw [← closure_union, ← Set.image_union] at this
       -- specialize this (exact?%)
-
-
       sorry
       ,x.val.isClosed.preimage_val⟩
+
+
+noncomputable def codim [TopologicalSpace X] (Y: IrreducibleCloseds X) : WithBot ℕ∞ :=
+    ⨆ (U: IrreducibleCloseds X), (eCodim Y U)
+
 
 lemma inter_irred_strictMono (hU : IsOpen U) : StrictMono (inter_irred hU) := by sorry
 
@@ -33,4 +39,7 @@ lemma closure_irred_bij {U : Set X} (U_open : IsOpen U) (U_nonempty : U.Nonempty
 
 example (hU : IsOpen U) (a b : IrreducibleCloseds X) : (a -[(· < ·)]→* b) → (inter_irred hU ⟨a,sorry⟩) -[(· < ·)]→* (inter_irred hU ⟨b,sorry⟩) := by
   sorry
-  -- refine mapOrd _ (foo_strictMono hU)
+
+-- not sure if it's needed
+lemma codim_nonneg [TopologicalSpace X] (Y: IrreducibleCloseds X) : 0 ≤ codim Y := by
+  sorry
