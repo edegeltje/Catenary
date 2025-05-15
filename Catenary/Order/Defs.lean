f60b0e8b5fce082362ec84a295bec2c3085de2e2
Edward van de Meent <edwardvdmeent@gmail.com>
2025-05-15 11:32:19 +0200

    stuffs

--- Catenary/Order/Defs.lean
+++ Catenary/Order/Defs.lean
@@ -1,5 +1,6 @@
 import Catenary.RelSeriesHT.Codim
-open scoped RelSeriesHT
+
+open RelSeriesHT
 abbrev IsCatenaryOrder (α : Type*) [Preorder α] : Prop := Rel.IsCatenary (LT.lt : Rel α α)
 
 abbrev IsDiscreteOrder (α : Type*) [Preorder α] : Prop := Rel.IsDiscrete (LT.lt : Rel α α)
@@ -7,4 +8,8 @@ abbrev IsDiscreteOrder (α : Type*) [Preorder α] : Prop := Rel.IsDiscrete (LT.l
 noncomputable def eCodim {α : Type*} [Preorder α] (a b : α) : WithBot ℕ∞ := Rel.eCodim LT.lt a b
 
 lemma isCatenaryOrder_iff_isDiscreteOrder_and_dimension_formula (α : Type*) [Preorder α]: IsCatenaryOrder α ↔ IsDiscreteOrder α ∧
-    ∀ {a b c: α }, (a -[(· < ·)]→* b) → (b -[(· < ·)]→* c) →  eCodim a b + eCodim b c = eCodim a c := RelSeriesHT.isCatenary_iff_isDiscrete_and_dimension_formula
+    ∀ {a b c : α }, (a -[(· < ·)]→* b) → (b -[(· < ·)]→* c) → eCodim a b + eCodim b c = eCodim a c := RelSeriesHT.isCatenary_iff_isDiscrete_and_dimension_formula
+
+def mapOrd {α : Type*} [Preorder α] {β : Type*} [Preorder β] (f : α → β) (hf : StrictMono f) {a b : α} : (a -[(· < ·)]→* b) → (f a -[(· < ·)]→* (f b))
+  | .singleton a => .singleton (f a)
+  | .cons a b hab => .cons (f a) (mapOrd f hf b) (hf hab)
