33617a81d86531597494a393c747b1f7f9a85e56
Alvise Sembenico <alvise.sembenico@gmail.com>
2025-05-15 16:51:08 +0200

    add closure stricly mono

--- Catenary/Topology/Codim.lean
+++ Catenary/Topology/Codim.lean
@@ -7,6 +7,9 @@ import Catenary.Order.Defs
 open TopologicalSpace Set.Notation RelSeriesHT
 variable {X : Type*} [TopologicalSpace X] {U : Set X}
 
+
+-- General results
+
 lemma closure_irred_of_irred {U : Set X} (U_nonempty : U.Nonempty) (T : Set U) (T_irred : IsIrreducible T) : IsIrreducible (closure (X := X) T) := by
  sorry
 
@@ -29,14 +32,20 @@ def inter_irred (hU : IsOpen U) : {s : (IrreducibleCloseds X) // (U ↓∩ s).No
       ,x.val.isClosed.preimage_val⟩
 
 
-noncomputable def codim [TopologicalSpace X] (Y: IrreducibleCloseds X) : WithBot ℕ∞ :=
-    ⨆ (U: IrreducibleCloseds X), (eCodim Y U)
 
 
 lemma inter_irred_strictMono (hU : IsOpen U) : StrictMono (inter_irred hU) := by sorry
 
 lemma closure_irred_bij {U : Set X} (U_open : IsOpen U) (U_nonempty : U.Nonempty) : Function.Bijective (closure_irred U_nonempty U_open):= sorry
 
+lemma closure_strict_mono_on_irreducible_closed {U : Set X} {A B: IrreducibleCloseds U}  (r: A < B) : (closure A.carrier) < (closure B) := by
+  sorry
+
+--
+-- Codimensions related
+
+noncomputable def codim [TopologicalSpace X] (Y: IrreducibleCloseds X) : WithBot ℕ∞ :=
+    ⨆ (U: IrreducibleCloseds X), (eCodim Y U)
 example (hU : IsOpen U) (a b : IrreducibleCloseds X) : (a -[(· < ·)]→* b) → (inter_irred hU ⟨a,sorry⟩) -[(· < ·)]→* (inter_irred hU ⟨b,sorry⟩) := by
   sorry
 
