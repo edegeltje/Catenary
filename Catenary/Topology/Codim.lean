c412e78d036ba086503b0a6a062516dedd541229
Jacob Scharmberg <hi@jacobscharmberg.net>
2025-05-17 16:22:33 +0200

    make closure_irred into RelIso

--- Catenary/Topology/Codim.lean
+++ Catenary/Topology/Codim.lean
@@ -11,14 +11,12 @@ variable {X : Type*} [TopologicalSpace X] {U : Set X}
 
 -- General results
 
-lemma closure_irred_of_irred {U : Set X} (U_nonempty : U.Nonempty) (T : Set U) (T_irred : IsIrreducible T) : IsIrreducible (closure (X := X) T) := by
+lemma closure_irred_of_irred {U : Set X} (T : Set U) (T_irred : IsIrreducible T) : IsIrreducible (closure (X := X) T) := by
  sorry
 
-def closure_irred {U : Set X} (U_nonempty : U.Nonempty) (U_open : IsOpen U) : IrreducibleCloseds U → {s : IrreducibleCloseds X // ((s : Set X) ∩ U).Nonempty} :=
-  fun T ↦ ⟨⟨closure (X := X) T.carrier, closure_irred_of_irred U_nonempty T.carrier T.is_irreducible', isClosed_closure⟩, sorry⟩
-
-def inter_irred (hU : IsOpen U) : {s : (IrreducibleCloseds X) // (U ↓∩ s).Nonempty } → IrreducibleCloseds U :=
-  fun x =>
+def closure_irred {U : Set X} (U_open : IsOpen U) : RelIso (α := IrreducibleCloseds U) (β := {s : IrreducibleCloseds X // (U ↓∩ s).Nonempty}) (· < ·) (· < ·) where
+  toFun := fun T ↦ ⟨⟨closure (X := X) T.carrier, closure_irred_of_irred T.carrier T.is_irreducible', isClosed_closure⟩, sorry⟩
+  invFun := fun x =>
     ⟨U ↓∩ x,by
       have := x.val.isIrreducible.isPreirreducible
       constructor
@@ -31,20 +29,16 @@ def inter_irred (hU : IsOpen U) : {s : (IrreducibleCloseds X) // (U ↓∩ s).No
       -- specialize this (exact?%)
       sorry
       ,x.val.isClosed.preimage_val⟩
-
-lemma inter_irred_strictMono (hU : IsOpen U) : StrictMono (inter_irred hU) := by sorry
-
-lemma closure_irred_bij {U : Set X} (U_open : IsOpen U) (U_nonempty : U.Nonempty) : Function.Bijective (closure_irred U_nonempty U_open):= sorry
+  left_inv := sorry
+  right_inv := sorry
+  map_rel_iff' := sorry
 
 lemma closure_strict_mono_on_irreducible_closed {U : Set X} {A B: IrreducibleCloseds U}  (r: A < B) : (closure A.carrier) < (closure B) := by
   sorry
 
-
 --
 -- Sup and bijection
 
-variable {X : Type*} [TopologicalSpace X]
-variable {U : Set X}
 
 lemma l {A: Set X} {B: Set X} (T: A → B) (f: X → Nat) (hT: Bijective T) (hf: ∀ a : A, f a = f (T a) ):
   -- f '' A = f '' (Subtype.val '' (T '' (Set.univ : Set (Subtype A)))) :=
@@ -73,7 +67,7 @@ lemma foo_strictMono: StrictMono (closure_of_irreducible_subset : IrreducibleClo
 noncomputable def codim [TopologicalSpace X] (Y: IrreducibleCloseds X) : WithBot ℕ∞ :=
     ⨆ (U: IrreducibleCloseds X), (eCodim Y U)
 
-example (hU : IsOpen U) (a b : IrreducibleCloseds X) : (a -[(· < ·)]→* b) → (inter_irred hU ⟨a,sorry⟩) -[(· < ·)]→* (inter_irred hU ⟨b,sorry⟩) := by
+example (hU : IsOpen U) (a b : IrreducibleCloseds X) : (a -[(· < ·)]→* b) → ((closure_irred hU).invFun ⟨a,sorry⟩) -[(· < ·)]→* ((closure_irred hU).invFun ⟨b,sorry⟩) := by
   sorry
 
 -- not sure if it's needed
